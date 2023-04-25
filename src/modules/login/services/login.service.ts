bb7e9ab218ae1e81fa3440d759c6fe7708f764e8
Juliogomezdev <julio.gomez11@inacapmail.cl>
2023-04-25 18:54:46 -0400

    Se agregan api para crear y iniciar sesion

--- src/modules/login/services/login.service.ts
+++ src/modules/login/services/login.service.ts
@@ -1,22 +1,28 @@
 import { BadRequestException, HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import LoginEntity from '../models/entities/login.entity';
-import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
+import { Repository } from 'typeorm';
+import { JwtService } from '@nestjs/jwt';
+import { LoginEntity } from '../models/entities/login.entity';
+import { ProfileEntity } from 'src/modules/profile/models/entities/profile.entity';
+
 
 @Injectable()
 export class LoginService {
-
   constructor(
     @InjectRepository(LoginEntity)  
-    private LoginReposository: Repository<LoginEntity>
+    private LoginReposository: Repository<LoginEntity>,
+    @InjectRepository(ProfileEntity)
+    private PersonaReposository: Repository<ProfileEntity>,
+    private jwtService: JwtService
   ){}
 
+  // Crea un usuario
   async createLogin(registerBody): Promise<any>{
 
     const existsUser = await this.LoginReposository.findOne({where: {email: registerBody.email}});
     if(existsUser){
-      
+     
       throw new HttpException('Usuario ya existente', HttpStatus.ACCEPTED);
     }
     else {
@@ -45,4 +51,46 @@ export class LoginService {
       throw new BadRequestException('Hubo un error', { cause: new Error() });
     }
   }
+
+  // Inicio de sesion
+  async sessionLogin(sessionBody): Promise<any>{
+
+    const validateUser = await this.LoginReposository.findOne(
+      {
+        where:{
+          email: sessionBody.email
+        }
+      }
+    )
+
+    if (validateUser) {
+      const isMatch = await bcrypt.compare(sessionBody.clave, validateUser.clave);
+
+      if(isMatch){
+        const token =  this.jwtService.sign({userId: validateUser.id})
+        const getPersona = await this.PersonaReposository.findOne({where:{fk_login: validateUser.id }})
+        if(!getPersona){
+          return {
+            "id": validateUser.id,
+            "estado": 0
+          }
+        }
+
+      // Obtiene nombre y apellido hasta el primer espacio, luego concatene y deja el primer nombre y primer apellido
+        const namePersona = getPersona.nombre.split(' ').shift()+' '+getPersona.apellido.split(' ').shift();
+        const newBody = {
+          "estado": validateUser.estado,
+          "estatus_registro": validateUser.estatus_registro,
+          "tipo_perfil":validateUser.tipo_perfil,
+          "token":token,
+          "nombre":namePersona
+        }
+        return newBody
+      }
+    }
+    else {
+      return "Usuario no encontrado"
+    }
+  }
+
 }
---
+++ src/modules/profile/models/entities/profile.entity.ts
@@ -0,0 +1,43 @@
+import LoginEntity from "src/modules/login/models/entities/login.entity";
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, OneToOne, JoinColumn, CreateDateColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('persona')
+export class ProfileEntity extends BaseEntity {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  nombre: string;
+
+  @Column()
+  apellido: string;
+
+  @Column()
+  edad: number;
+
+  @Column()
+  profesion: string;
+
+  @Column()
+  telefono: string;
+
+  @Column()
+  region: string;
+
+  @Column()
+  comuna: string;
+
+  @Column()
+  fecha_creacion: Date;
+
+  @Column()
+  fecha_modificacion: Date;
+
+  @Column()
+  fk_login: number;
+
+  @OneToOne(() => LoginEntity)
+  @JoinColumn({ name: 'fk_login' })
+  login: LoginEntity;
+}
